5ab48c30d32ba38908f6d9cf032c1622f8ad0029
AlexYisus <alextaraguay20@gmail.com>
2024-08-31 18:21:27 -0500

    Update bat

--- build.sh
+++ build.sh
@@ -4,11 +4,17 @@ set -o errexit  # Exit on first command that fails
 # Install Python dependencies
 pip install -r ./backend/requirements.txt
 
+# Navigate to the React app directory
+cd ./Frontend
+
 # Install Node.js dependencies
-npm install # Reemplaza "path/to/your/react-app" con la ruta real a tu aplicación React
+npm install
 
 # Build React app
-npm run build -prefix ./Frontend # Reemplaza "path/to/your/react-app" con la ruta real a tu aplicación React
+npm run build
+
+# Navigate back to the Django directory
+cd ..
 
 # Collect static files
 python manage.py collectstatic --no-input
